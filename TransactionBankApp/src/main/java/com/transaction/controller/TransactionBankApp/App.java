a991440b62947b870e311960698cbdef1bd9c2b3
kishorikaranjule <129212126+kishorikaranjule@users.noreply.github.com>
2024-03-30 12:11:11 -0700

    Add files via upload

---
+++ TransactionBankApp/src/main/java/com/transaction/controller/TransactionBankApp/App.java
@@ -0,0 +1,13 @@
+package com.transaction.controller.TransactionBankApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
